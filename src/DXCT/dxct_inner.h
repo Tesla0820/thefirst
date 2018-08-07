a102ac602a5ab9c532d30aa31ae8a99e40ecb611
Tesla0129 <s0820@KITSUNE>
2018-08-08 00:10:47 +0900

    セーフリリースマクロの追加
    共通で利用するため、innerヘッダに追加

--- src/DXCT/dxct_inner.h
+++ src/DXCT/dxct_inner.h
@@ -6,13 +6,28 @@
 #ifndef HEADER_DXCT_INNER_H
 #define HEADER_DXCT_INNER_H
 
+//共通インクルードヘッダ
 #include <Windows.h>
 #include <d3d9.h>
 #include <d3dx9.h>
 
-
 //内部定義
-#define DXCT_INTERFACE	struct
-#define DXCT_ABSTRACT	class
+#ifndef DXCT_INTERFACE
+#	define DXCT_INTERFACE	struct
+#endif
+#ifndef DXCT_ABSTRACT
+#	define DXCT_ABSTRACT	class
+#endif 
+
+//セーフリリースマクロ
+#ifndef SAFE_DELETE
+#	define SAFE_DELETE(p)       { if (p) { delete (p);     (p)=NULL; } }
+#endif    
+#ifndef SAFE_DELETE_ARRAY
+#	define SAFE_DELETE_ARRAY(p) { if (p) { delete[] (p);   (p)=NULL; } }
+#endif    
+#ifndef SAFE_RELEASE
+#	define SAFE_RELEASE(p)      { if (p) { (p)->Release(); (p)=NULL; } }
+#endif
 
 #endif
