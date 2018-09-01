513b21ee5c221cba48af9925508419310dd61e43
Tesla0820 <42001863+Tesla0820@users.noreply.github.com>
2018-09-01 14:28:44 +0900

    stage読み込み部追加

---
+++ src/Game/GameScene/StageLoader.cpp
@@ -0,0 +1,10 @@
+//
+//		ƒtƒ@ƒCƒ‹–¼:StageLoader.cpp
+//
+
+
+#include "StageLoader.h"
+
+void Game::StageLoader::LoadStage(std::string file)
+{
+}
---
+++ src/Game/GameScene/StageLoader.h
@@ -0,0 +1,23 @@
+//
+//		ƒtƒ@ƒCƒ‹–¼:StageLoader.h
+//
+
+#ifndef HEADER_STAGE_LOADER_H
+#define HEADER_STAGE_LOADER_H
+
+#include <string>
+
+namespace Game
+{
+	class StageLoader
+	{
+		private:
+			
+
+		public:
+			static void LoadStage(std::string file);
+
+	};
+}
+
+#endif
