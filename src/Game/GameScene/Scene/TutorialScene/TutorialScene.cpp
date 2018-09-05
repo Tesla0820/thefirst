47fac3130a83cf49f9e39a005aad5645ceddfc50
Suzukenn <Suzukenn@users.noreply.github.com>
2018-09-05 11:33:05 +0900

    コミット

--- src/Game/GameScene/Scene/TutorialScene/TutorialScene.cpp
+++ src/Game/GameScene/Scene/TutorialScene/TutorialScene.cpp
@@ -52,25 +52,28 @@ bool TutorialScene::Init()
 
   //ステージ
   StageLoader::LoadStage("./data/stage/stage0.txt");
+
+  GimmickFactory::InstantiateBronzeStatue(&D3DXVECTOR3(-100.0f, 0.0f, 0.0f), &D3DXVECTOR3(0.0f, 90.0f, 0.0f));
+
   
-  GimmickFactory::InstantiateBrokenPillar(&D3DXVECTOR3(-100.0f, 0.0f, 0.0f), &D3DXVECTOR3(0.0f, 90.0f, 0.0f));
-  GimmickFactory::InstantiateArmor(&D3DXVECTOR3(-410.0f, 0.0f, -60.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
+  //GimmickFactory::InstantiateBrokenPillar(&D3DXVECTOR3(-100.0f, 0.0f, 0.0f), &D3DXVECTOR3(0.0f, 90.0f, 0.0f));
+  //GimmickFactory::InstantiateArmor(&D3DXVECTOR3(-410.0f, 0.0f, -60.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
 
-  GimmickFactory::InstantiatePillar(&D3DXVECTOR3(-450.0f, 0.0f, 40.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
-  GimmickFactory::InstantiatePillar(&D3DXVECTOR3(-450.0f, 0.0f, -60.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
-  GimmickFactory::InstantiateArmor(&D3DXVECTOR3(-510.0f, 0.0f, 40.0f), &D3DXVECTOR3(0.0f, 180.0f, 0.0f));
-  GimmickFactory::InstantiateArmor(&D3DXVECTOR3(-510.0f, 0.0f, -60.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
-  GimmickFactory::InstantiatePillar(&D3DXVECTOR3(-590.0f, 0.0f, 40.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
-  GimmickFactory::InstantiatePillar(&D3DXVECTOR3(-590.0f, 0.0f, -60.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
+  //GimmickFactory::InstantiatePillar(&D3DXVECTOR3(-450.0f, 0.0f, 40.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
+  //GimmickFactory::InstantiatePillar(&D3DXVECTOR3(-450.0f, 0.0f, -60.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
+  //GimmickFactory::InstantiateArmor(&D3DXVECTOR3(-510.0f, 0.0f, 40.0f), &D3DXVECTOR3(0.0f, 180.0f, 0.0f));
+  //GimmickFactory::InstantiateArmor(&D3DXVECTOR3(-510.0f, 0.0f, -60.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
+  //GimmickFactory::InstantiatePillar(&D3DXVECTOR3(-590.0f, 0.0f, 40.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
+  //GimmickFactory::InstantiatePillar(&D3DXVECTOR3(-590.0f, 0.0f, -60.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
 
 
-  GimmickFactory::InstantiateThorns(&D3DXVECTOR3(-800.0f, 0.0f, 45.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
-  GimmickFactory::InstantiateThorns(&D3DXVECTOR3(-800.0f, 0.0f, -62.0f), &D3DXVECTOR3(0.0f, 180.0f, 0.0f));
+  //GimmickFactory::InstantiateThorns(&D3DXVECTOR3(-800.0f, 0.0f, 45.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
+  //GimmickFactory::InstantiateThorns(&D3DXVECTOR3(-800.0f, 0.0f, -62.0f), &D3DXVECTOR3(0.0f, 180.0f, 0.0f));
 
-  GimmickFactory::InstantiatePillar(&D3DXVECTOR3(-890.0f, 0.0f, 40.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
-  GimmickFactory::InstantiatePillar(&D3DXVECTOR3(-890.0f, 0.0f, -60.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
-  
-  GimmickFactory::InstantiateBell(&D3DXVECTOR3(-970.0f, 0.0f, 0.0f), &D3DXVECTOR3(0.0f, 90.0f, 0.0f));
+  //GimmickFactory::InstantiatePillar(&D3DXVECTOR3(-890.0f, 0.0f, 40.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
+  //GimmickFactory::InstantiatePillar(&D3DXVECTOR3(-890.0f, 0.0f, -60.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f));
+  //
+  //GimmickFactory::InstantiateBell(&D3DXVECTOR3(-970.0f, 0.0f, 0.0f), &D3DXVECTOR3(0.0f, 90.0f, 0.0f));
 
   // UIゲージの枠
   GameFactory::CreateUIgaugeEdge();
